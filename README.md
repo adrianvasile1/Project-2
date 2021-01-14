c9b1467e1495b98469a16cb3749f227de514621c
adrian.vasile087 <adrian.vasile087@gmail.com>
2021-01-14 16:34:03 +0100

    com2
    com2

---
+++ README.md
@@ -0,0 +1,4 @@
+Landing page
+- Splited in multiple sections News,Contacts ,About and Services
+- Basic functionalites like header blocked when scroll or dynamic sidebar 
+- No clours added 
--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
+//When scroll the topnav is fixed .
 window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
+const navbar = document.getElementById("navbar");
+const sticky = navbar.offsetTop;
 
 function myFunction() {
   if (window.pageYOffset >= sticky) {
@@ -10,7 +10,7 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
-
+// This function open and close the sidenav also is changing the color of the background when is activated .
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("navbar").style.marginLeft = "250px";
@@ -26,3 +26,14 @@ function closeNav() {
   document.body.style.backgroundColor = "white";
   document.body.style.color= "black";
 }
+
+//When is clicked on the sidenav, the following function scrolls to the paragraph 
+links = [ ...document.getElementsByClassName("para")]
+links.map(element => {
+  element.addEventListener("click", e => {
+    e.preventDefault()
+    document.getElementById(e.target.dataset.target).scrollIntoView({
+      behavior: "smooth", block: "end", inline: "nearest"
+    })
+  })
+})
