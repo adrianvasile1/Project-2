8620e4089f646cbad2efe17428ddacc9920a595e
adrian.vasile087 <adrian.vasile087@gmail.com>
2021-01-15 15:57:40 +0100

    com4
    com4

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 //When scroll the topnav is fixed .
-window.onscroll = function() {myFunction()};
-const navbar = document.getElementById("navbar");
+const navbar = document.getElementById("nav-container");
 const sticky = navbar.offsetTop;
+window.onscroll = function() {myFunction()};
+
 
 function myFunction() {
   if (window.pageYOffset >= sticky) {
@@ -10,6 +11,33 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+//Change navigation style on scroll
+window.addEventListener('scroll', event => { 
+  let nav = document.querySelector('.nav-container'); 
+  
+  (window.scrollY >= 10) ? nav.classList.add('scroll') : nav.classList.remove('scroll');
+});
+
+//Active navigation on scroll
+window.addEventListener('scroll', event => {
+let navigationLinks = document.querySelectorAll('nav ul li a');
+let fromTop = window.scrollY;
+
+navigationLinks.forEach(link => {
+  let section = document.querySelector(link.hash);
+ 
+  if (
+    section.offsetTop <= fromTop &&
+    section.offsetTop + section.offsetHeight > fromTop
+  ) {
+    link.classList.add('active');
+  } else {
+    link.classList.remove('active');
+  }
+});
+});
+
 // This function open and close the sidenav also is changing the color of the background when is activated .
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
@@ -28,6 +56,7 @@ function closeNav() {
 }
 
 //When is clicked on the sidenav, the following function scrolls to the paragraph 
+
 links = [ ...document.getElementsByClassName("para")]
 links.map(element => {
   element.addEventListener("click", e => {
@@ -38,21 +67,3 @@ links.map(element => {
   })
 })
 
-//active nav on scroll
-window.addEventListener('scroll',event => {
-  let navigationLinks = document.querySelectorAll('navbar a');
-  let fromTop = window.scrollY;
-
-  navigationLinks.forEach(link => {
-    let section = document.querySelectorAll(link,hash);
-
-    if (
-      section.offsetTop <= fromTop &&
-      section.offsetTop + section.offsetHeight > fromTop
-    ) {
-      link.classList.add('active');
-    }else {
-      link.classList.remove('active');
-    }
-    });
-  });
